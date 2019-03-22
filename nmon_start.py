ddebbc37bfa1d362d7f10833f909905e12d6677c
Konstantin Yovchev <33600562+zinobg@users.noreply.github.com>
2019-03-22 14:32:22 +0100

    Update nmon_start.py

--- nmon_start.py
+++ nmon_start.py
@@ -16,8 +16,8 @@ nmon_exe="/usr/bin/nmon"                  # nmon executable
 nmon_sleep_seconds="300"                  # sleep time between nmon iterations
 nmon_stats_count="288"                    # number or nmon iterations
 nmon_file_mask="RPI"                      # output filename mask
-rasp_nmon_dir="/nmon/logs_test"                # output direcroty for Linux server
-days_to_keep=30                         # how many days to keep *.nmon.gz files, before delete them
+rasp_nmon_dir="/nmon/logs"                # output direcroty for Linux server
+days_to_keep=30                           # how many days to keep *.nmon.gz files, before delete them
 
 def check_output_file(file_part):
     count=1
